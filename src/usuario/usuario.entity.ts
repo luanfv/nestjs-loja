ad65f907cc8abb42992eb902eea4f0937ec0424f
Luan Vieira <luan.vieira@lambda3.com.br>
2023-12-24 06:42:56 -0300

    config: user entity

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,6 +1,30 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'usuarios' })
 export class UsuarioEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false })
   nome: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
   email: string;
+
+  @Column({ name: 'senha', length: 255, nullable: false })
   senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @CreateDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
